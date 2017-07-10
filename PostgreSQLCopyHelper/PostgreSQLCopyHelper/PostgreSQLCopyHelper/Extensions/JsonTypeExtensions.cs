c06c5e020c6b7d2c328f8087c72d11ea1e1d7267
Bostjan Rihter <bostjan.rihter@gmail.com>
2017-07-10 21:08:24 +0200

    JsonTypeExtensions added.

---
+++ PostgreSQLCopyHelper/PostgreSQLCopyHelper/PostgreSQLCopyHelper/Extensions/JsonTypeExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using NpgsqlTypes;
+
+namespace PostgreSQLCopyHelper
+{
+    public static class JsonTypeExtensions
+    {
+        public static PostgreSQLCopyHelper<TEntity> MapJson<TEntity>(this PostgreSQLCopyHelper<TEntity> helper, string columnName, Func<TEntity, DateTime> propertyGetter)
+        {
+            return helper.Map(columnName, propertyGetter, NpgsqlDbType.Json);
+        }
+
+        public static PostgreSQLCopyHelper<TEntity> MapJsonb<TEntity>(this PostgreSQLCopyHelper<TEntity> helper, string columnName, Func<TEntity, DateTime> propertyGetter)
+        {
+            return helper.Map(columnName, propertyGetter, NpgsqlDbType.Jsonb);
+        }
+    }
+}
