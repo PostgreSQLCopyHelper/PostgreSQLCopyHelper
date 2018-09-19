dfae55fc1afe109eca90185bdd1554f1526f0939
say25 <say25@cornell.edu>
2018-09-18 19:37:35 -0500

    Tests Validating Record Return

--- PostgreSQLCopyHelper/PostgreSQLCopyHelper/PostgreSQLCopyHelper.Test/ArrayTypesTest.cs
+++ PostgreSQLCopyHelper/PostgreSQLCopyHelper/PostgreSQLCopyHelper.Test/ArrayTypesTest.cs
@@ -66,12 +66,13 @@ namespace PostgreSQLCopyHelper.Test
                 Array = new[] { "A", "B" }
             };
 
-            subject.SaveAll(connection, new[] { entity0 });
+            var recordsSaved = subject.SaveAll(connection, new[] { entity0 });
 
             var result = GetAll();
 
             // Check if we have the amount of rows:
             Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(1, recordsSaved);
 
             // Check if the Result is not null:
             Assert.IsNotNull(result[0][0]);
